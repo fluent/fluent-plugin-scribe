bb5f2e8b8665ff4394c5feb64d7e748964631a22
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-09-04 19:14:12 +0900

    good-bye curious indentation!

--- lib/fluent/plugin/in_scribe.rb
+++ lib/fluent/plugin/in_scribe.rb
@@ -16,144 +16,142 @@
 #    limitations under the License.
 #
 module Fluent
+  class ScribeInput < Input
+    Plugin.register_input('scribe', self)
 
-class ScribeInput < Input
-  Plugin.register_input('scribe', self)
+    config_param :port,            :integer, :default => 1463
+    config_param :bind,            :string,  :default => '0.0.0.0'
+    config_param :server_type,     :string,  :default => 'nonblocking'
+    config_param :is_framed,       :bool,    :default => true
+    config_param :body_size_limit, :size,    :default => 32*1024*1024  # TODO default
+    config_param :add_prefix,      :string,  :default => nil
+    config_param :remove_newline,  :bool,    :default => false
+    config_param :msg_format,      :string,  :default => 'text'
 
-  config_param :port,            :integer, :default => 1463
-  config_param :bind,            :string,  :default => '0.0.0.0'
-  config_param :server_type,     :string,  :default => 'nonblocking'
-  config_param :is_framed,       :bool,    :default => true
-  config_param :body_size_limit, :size,    :default => 32*1024*1024  # TODO default
-  config_param :add_prefix,      :string,  :default => nil
-  config_param :remove_newline,  :bool,    :default => false
-  config_param :msg_format,      :string,  :default => 'text'
-
-  def initialize
-    require 'cgi'
-    require 'thrift'
-    $:.unshift File.join(File.dirname(__FILE__), 'thrift')
-    require 'fb303_types'
-    require 'fb303_constants'
-    require 'facebook_service'
-    require 'scribe_types'
-    require 'scribe_constants'
-    require 'scribe'
-    super
-  end
-
-  def configure(conf)
-    super
-  end
-
-  def start
-    $log.debug "listening scribe on #{@bind}:#{@port}"
-
-    handler = FluentScribeHandler.new
-    handler.add_prefix = @add_prefix
-    handler.remove_newline = @remove_newline
-    handler.msg_format = @msg_format
-    processor = Scribe::Processor.new handler
-
-    @transport = Thrift::ServerSocket.new @bind, @port
-    if @is_framed
-      transport_factory = Thrift::FramedTransportFactory.new
-    else
-      transport_factory = Thrift::BufferedTransportFactory.new
+    def initialize
+      require 'cgi'
+      require 'thrift'
+      $:.unshift File.join(File.dirname(__FILE__), 'thrift')
+      require 'fb303_types'
+      require 'fb303_constants'
+      require 'facebook_service'
+      require 'scribe_types'
+      require 'scribe_constants'
+      require 'scribe'
+      super
     end
 
-    unless ['text', 'json', 'url_param'].include? @msg_format
-      raise 'Unknown format: msg_format=#{@msg_format}'
+    def configure(conf)
+      super
     end
 
-    # 2011/09/29 Kazuki Ohta <kazuki.ohta@gmail.com>
-    # This section is a workaround to set strict_read and strict_write option.
-    # Ruby-Thrift 0.7 set them both 'true' in default, but Scribe protocol set
-    # them both 'false'.
-    protocol_factory = Thrift::BinaryProtocolFactory.new
-    protocol_factory.instance_eval {|obj|
-      def get_protocol(trans) # override
-        return Thrift::BinaryProtocol.new(trans,
-                                          strict_read=false,
-                                          strict_write=false)
-      end
-    }
+    def start
+      $log.debug "listening scribe on #{@bind}:#{@port}"
 
-    case @server_type
-    when 'simple'
-      @server = Thrift::SimpleServer.new processor, @transport, transport_factory, protocol_factory
-    when 'threaded'
-      @server = Thrift::ThreadedServer.new processor, @transport, transport_factory, protocol_factory
-    when 'thread_pool'
-      @server = Thrift::ThreadPoolServer.new processor, @transport, transport_factory, protocol_factory
-    when 'nonblocking'
-      @server = Thrift::NonblockingServer.new processor, @transport, transport_factory, protocol_factory
-    else
-      raise ConfigError, "in_scribe: unsupported server_type '#{@server_type}'"
-    end
-    @thread = Thread.new(&method(:run))
-  end
+      handler = FluentScribeHandler.new
+      handler.add_prefix = @add_prefix
+      handler.remove_newline = @remove_newline
+      handler.msg_format = @msg_format
+      processor = Scribe::Processor.new handler
 
-  def shutdown
-    @transport.close unless @transport.closed?
-    #@thread.join # TODO
-  end
-
-  def run
-    @server.serve
-  rescue
-    $log.error "unexpected error", :error=>$!.to_s
-    $log.error_backtrace
-  end
-
-  class FluentScribeHandler
-    attr_accessor :add_prefix
-    attr_accessor :remove_newline
-    attr_accessor :msg_format
-
-    def Log(msgs)
-      begin
-        msgs.each { |msg|
-          record = create_record(msg)
-          if @add_prefix
-            Engine.emit(@add_prefix + '.' + msg.category, Engine.now, record)
-          else
-            Engine.emit(msg.category, Engine.now, record)
-          end
-        }
-        return ResultCode::OK
-      rescue => e
-        $log.error "unexpected error", :error=>$!.to_s
-        $log.error_backtrace
-        return ResultCode::TRY_LATER
-      end
-    end
-
-    private
-    def create_record(msg)
-      case @msg_format
-      when 'text'
-        if @remove_newline
-          return { 'message' => msg.message.force_encoding('UTF-8').chomp }
-        else
-          return { 'message' => msg.message.force_encoding('UTF-8') }
-        end
-      when 'json'
-        js = JSON.parse(msg.message.force_encoding('UTF-8'))
-        raise 'body must be a Hash, if json_body=true' unless js.is_a?(Hash)
-        return js
-      when 'url_param'
-        s = msg.message.force_encoding('UTF-8')
-        return Hash[ s.split('&').map { |kv|
-            k,v = kv.split('=', 2);
-            [CGI.unescape(k), CGI.unescape(v)]
-          }
-        ]
+      @transport = Thrift::ServerSocket.new @bind, @port
+      if @is_framed
+        transport_factory = Thrift::FramedTransportFactory.new
       else
-        raise 'Invalid format: #{@msg_format}'
+        transport_factory = Thrift::BufferedTransportFactory.new
+      end
+
+      unless ['text', 'json', 'url_param'].include? @msg_format
+        raise 'Unknown format: msg_format=#{@msg_format}'
+      end
+
+      # 2011/09/29 Kazuki Ohta <kazuki.ohta@gmail.com>
+      # This section is a workaround to set strict_read and strict_write option.
+      # Ruby-Thrift 0.7 set them both 'true' in default, but Scribe protocol set
+      # them both 'false'.
+      protocol_factory = Thrift::BinaryProtocolFactory.new
+      protocol_factory.instance_eval {|obj|
+        def get_protocol(trans) # override
+          return Thrift::BinaryProtocol.new(trans,
+            strict_read=false,
+            strict_write=false)
+        end
+      }
+
+      case @server_type
+      when 'simple'
+        @server = Thrift::SimpleServer.new processor, @transport, transport_factory, protocol_factory
+      when 'threaded'
+        @server = Thrift::ThreadedServer.new processor, @transport, transport_factory, protocol_factory
+      when 'thread_pool'
+        @server = Thrift::ThreadPoolServer.new processor, @transport, transport_factory, protocol_factory
+      when 'nonblocking'
+        @server = Thrift::NonblockingServer.new processor, @transport, transport_factory, protocol_factory
+      else
+        raise ConfigError, "in_scribe: unsupported server_type '#{@server_type}'"
+      end
+      @thread = Thread.new(&method(:run))
+    end
+
+    def shutdown
+      @transport.close unless @transport.closed?
+      #@thread.join # TODO
+    end
+
+    def run
+      @server.serve
+    rescue
+      $log.error "unexpected error", :error=>$!.to_s
+      $log.error_backtrace
+    end
+
+    class FluentScribeHandler
+      attr_accessor :add_prefix
+      attr_accessor :remove_newline
+      attr_accessor :msg_format
+
+      def Log(msgs)
+        begin
+          msgs.each { |msg|
+            record = create_record(msg)
+            if @add_prefix
+              Engine.emit(@add_prefix + '.' + msg.category, Engine.now, record)
+            else
+              Engine.emit(msg.category, Engine.now, record)
+            end
+          }
+          return ResultCode::OK
+        rescue => e
+          $log.error "unexpected error", :error=>$!.to_s
+          $log.error_backtrace
+          return ResultCode::TRY_LATER
+        end
+      end
+
+      private
+      def create_record(msg)
+        case @msg_format
+        when 'text'
+          if @remove_newline
+            return { 'message' => msg.message.force_encoding('UTF-8').chomp }
+          else
+            return { 'message' => msg.message.force_encoding('UTF-8') }
+          end
+        when 'json'
+          js = JSON.parse(msg.message.force_encoding('UTF-8'))
+          raise 'body must be a Hash, if json_body=true' unless js.is_a?(Hash)
+          return js
+        when 'url_param'
+          s = msg.message.force_encoding('UTF-8')
+          return Hash[ s.split('&').map { |kv|
+              k,v = kv.split('=', 2);
+              [CGI.unescape(k), CGI.unescape(v)]
+            }
+          ]
+        else
+          raise 'Invalid format: #{@msg_format}'
+        end
       end
     end
   end
-end
-
 end
--- lib/fluent/plugin/out_scribe.rb
+++ lib/fluent/plugin/out_scribe.rb
@@ -16,99 +16,97 @@
 #    limitations under the License.
 #
 module Fluent
+  class ScribeOutput < BufferedOutput
+    Fluent::Plugin.register_output('scribe', self)
 
-class ScribeOutput < BufferedOutput
-  Fluent::Plugin.register_output('scribe', self)
+    config_param :host,      :string,  :default => 'localhost'
+    config_param :port,      :integer, :default => 1463
+    config_param :field_ref, :string,  :default => 'message'
+    config_param :timeout,   :integer, :default => 30
 
-  config_param :host,      :string,  :default => 'localhost'
-  config_param :port,      :integer, :default => 1463
-  config_param :field_ref, :string,  :default => 'message'
-  config_param :timeout,   :integer, :default => 30
+    config_param :remove_prefix,    :string,  :default => nil
+    config_param :add_newline,      :bool,    :default => false
+    config_param :default_category, :string,  :default => 'unknown'
+    config_param :format_to_json,   :bool,    :default => false
 
-  config_param :remove_prefix,    :string,  :default => nil
-  config_param :add_newline,      :bool,    :default => false
-  config_param :default_category, :string,  :default => 'unknown'
-  config_param :format_to_json,   :bool,    :default => false
-
-  def initialize
-    require 'thrift'
-    $:.unshift File.join(File.dirname(__FILE__), 'thrift')
-    require 'fb303_types'
-    require 'fb303_constants'
-    require 'facebook_service'
-    require 'scribe_types'
-    require 'scribe_constants'
-    require 'scribe'
-    super
-  end
-
-  def configure(conf)
-    # override default buffer_chunk_limit
-    conf['buffer_chunk_limit'] ||= '1m'
-
-    super
-  end
-
-  def start
-    super
-
-    if @remove_prefix
-      @removed_prefix_string = @remove_prefix + '.'
-      @removed_length = @removed_prefix_string.length
+    def initialize
+      require 'thrift'
+      $:.unshift File.join(File.dirname(__FILE__), 'thrift')
+      require 'fb303_types'
+      require 'fb303_constants'
+      require 'facebook_service'
+      require 'scribe_types'
+      require 'scribe_constants'
+      require 'scribe'
+      super
     end
-  end
 
-  def shutdown
-    super
-  end
+    def configure(conf)
+      # override default buffer_chunk_limit
+      conf['buffer_chunk_limit'] ||= '1m'
 
-  def format(tag, time, record)
-    if @remove_prefix and
-        ( (tag[0, @removed_length] == @removed_prefix_string and tag.length > @removed_length) or
-          tag == @remove_prefix)
-      [(tag[@removed_length..-1] || @default_category), record].to_msgpack
-    else
-      [tag, record].to_msgpack
+      super
     end
-  end
 
-  def write(chunk)
-    socket = Thrift::Socket.new @host, @port, @timeout
-    transport = Thrift::FramedTransport.new socket
-    protocol = Thrift::BinaryProtocol.new transport, false, false
-    client = Scribe::Client.new protocol
+    def start
+      super
 
-    transport.open
-    begin
-      entries = []
-
-      chunk.msgpack_each do |arr|
-        tag, record = arr
-        next unless @format_to_json || record.has_key?(@field_ref)
-
-        message = @format_to_json ? record : record[@field_ref]
-
-        if message.kind_of?(Array) or message.kind_of?(Hash)
-          message = message.to_json
-        end
-
-        if @add_newline
-          message = message + "\n"
-        end
-
-        entry = LogEntry.new
-        entry.category = tag
-        entry.message = message.force_encoding('ASCII-8BIT')
-
-        entries << entry
+      if @remove_prefix
+        @removed_prefix_string = @remove_prefix + '.'
+        @removed_length = @removed_prefix_string.length
       end
+    end
 
-      $log.debug "Writing #{entries.count} entries to scribe"
-      client.Log(entries)
-    ensure
-      transport.close
+    def shutdown
+      super
+    end
+
+    def format(tag, time, record)
+      if @remove_prefix and
+          ( (tag[0, @removed_length] == @removed_prefix_string and tag.length > @removed_length) or
+          tag == @remove_prefix)
+        [(tag[@removed_length..-1] || @default_category), record].to_msgpack
+      else
+        [tag, record].to_msgpack
+      end
+    end
+
+    def write(chunk)
+      socket = Thrift::Socket.new @host, @port, @timeout
+      transport = Thrift::FramedTransport.new socket
+      protocol = Thrift::BinaryProtocol.new transport, false, false
+      client = Scribe::Client.new protocol
+
+      transport.open
+      begin
+        entries = []
+
+        chunk.msgpack_each do |arr|
+          tag, record = arr
+          next unless @format_to_json || record.has_key?(@field_ref)
+
+          message = @format_to_json ? record : record[@field_ref]
+
+          if message.kind_of?(Array) or message.kind_of?(Hash)
+            message = message.to_json
+          end
+
+          if @add_newline
+            message = message + "\n"
+          end
+
+          entry = LogEntry.new
+          entry.category = tag
+          entry.message = message.force_encoding('ASCII-8BIT')
+
+          entries << entry
+        end
+
+        $log.debug "Writing #{entries.count} entries to scribe"
+        client.Log(entries)
+      ensure
+        transport.close
+      end
     end
   end
-end
-
 end
